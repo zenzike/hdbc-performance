cc2b042964e5a750e75ae2fbb3e2e56eb6575225
Nicolas Wu <zenzike@gmail.com>
2011-07-05 17:06:26 +0100

    Adding benchmarks for Time and Date

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,9 @@ import Database.HDBC
 import Database.HDBC.ODBC
 import Database.HDBC.PostgreSQL
 
+import Data.Time.Calendar (Day, fromGregorian)
+import Data.Time.LocalTime (TimeOfDay(TimeOfDay), LocalTime(LocalTime))
+
 import Control.Monad (replicateM, forM, liftM)
 import Debug.Trace
 
@@ -42,13 +45,15 @@ benchBackend backend conn = bgroup backend
   -- , benchSelectInt32 conn 10000
   , benchSelectDouble conn 10000
   , benchSelectString conn 10000
+  , benchSelectDate conn 10000
+  , benchSelectTime conn 10000
   ]
 
 --------------------
 setupInsert :: IConnection conn => conn -> IO ()
 setupInsert conn = do 
   run conn
-    "CREATE TABLE testInsert (v1 INTEGER, v2 FLOAT, v3 CHAR(64))" []
+    "CREATE TABLE testInsert (v1 INTEGER, v2 FLOAT, v3 CHAR(4))" []
   commit conn
 
 benchInsert :: IConnection conn => conn -> Int -> Benchmark
@@ -73,44 +78,67 @@ teardownInsert conn = do
 setupSelect :: IConnection conn => conn -> Int -> IO ()
 setupSelect conn n = do
   run conn
-    "CREATE TABLE testSelect (v1 INTEGER, v2 FLOAT, v3 CHAR(64))" []
+    "CREATE TABLE testSelect (v1 INTEGER, v2 FLOAT, v3 CHAR(4), v4 DATE, v5 TIMESTAMP)" []
   replicateM n $
-    run conn "INSERT INTO testSelect (v1, v2, v3) VALUES (?, ?, ?)"
+    run conn "INSERT INTO testSelect (v1, v2, v3, v4, v5) VALUES (?, ?, ?, ?, ?)"
       [ SqlInt32 1
       , SqlDouble 1.0
       , SqlString "test"
+      , SqlLocalDate testDay
+      , SqlLocalTime testTime
       ]
   commit conn
 
+testDay :: Day
+testDay = fromGregorian 2011 11 20
+
+testTime :: LocalTime
+testTime = LocalTime testDay (TimeOfDay 3 14 15.926536) -- 5897932)
+
 benchSelect :: IConnection conn => conn -> Int -> Benchmark
 benchSelect conn n = bench "Select" $ nfIO $ do
-  quickQuery conn "SELECT * FROM testSelect LIMIT ?" [SqlInt32 (fromIntegral n)]
+  quickQuery' conn "SELECT * FROM testSelect LIMIT ?" [SqlInt32 (fromIntegral n)]
   commit conn
 
 benchSelectInt :: IConnection conn => conn -> Int -> Benchmark
 benchSelectInt conn n = bench "SelectInt" $ nfIO $ do
   vss <- quickQuery' conn "SELECT v1 FROM testSelect LIMIT ?" [SqlInt32 (fromIntegral n)]
-  if ((sum . map (\[v] -> fromSql v :: Int)) vss /= n)
-    then error "benchSelectInt: Unexpected sum!"
-    else return $ sum . map (\[v] -> fromSql v :: Int) $ vss
-
-benchSelectInt32 :: IConnection conn => conn -> Int -> Benchmark
-benchSelectInt32 conn n = bench "SelectInt32" $ nfIO $ do
-  vss <- quickQuery' conn "SELECT v1 FROM testSelect LIMIT ?" [SqlInt32 (fromIntegral n)]
   commit conn
-  return $ map (\[SqlInt32 v] -> v) vss
+  if ((sum . map (\[v] -> fromSql v :: Int)) vss == n)
+    then return ()
+    else error "benchSelectInt: Unexpected sum!"
 
 benchSelectDouble :: IConnection conn => conn -> Int -> Benchmark
 benchSelectDouble conn n = bench "SelectDouble" $ nfIO $ do
   vss <- quickQuery' conn "SELECT v2 FROM testSelect LIMIT ?" [SqlInt32 (fromIntegral n)]
   commit conn
-  return $ map (\[v] -> fromSql v :: Double) vss
+  if ((sum . map (\[v] -> fromSql v :: Double)) vss == fromIntegral n)
+    then return ()
+    else error "benchSelectDouble: Unexpected sum!"
 
 benchSelectString :: IConnection conn => conn -> Int -> Benchmark
 benchSelectString conn n = bench "SelectString" $ nfIO $ do
   vss <- quickQuery' conn "SELECT v3 FROM testSelect LIMIT ?" [SqlInt32 (fromIntegral n)]
   commit conn
-  return $ map (\[v] -> fromSql v :: String) vss
+  if all (\[v] -> (fromSql v :: String) == "test") vss
+    then return ()
+    else error "benchSelectString: Unexpected String!"
+
+benchSelectDate :: IConnection conn => conn -> Int -> Benchmark
+benchSelectDate conn n = bench "SelectDate" $ nfIO $ do
+  vss <- quickQuery' conn "SELECT v4 FROM testSelect LIMIT ?" [SqlInt32 (fromIntegral n)]
+  commit conn
+  if all (\[v] -> (fromSql v :: Day) == testDay) vss
+    then return ()
+    else error "benchSelectDate: Unexpected Date!"
+
+benchSelectTime :: IConnection conn => conn -> Int -> Benchmark
+benchSelectTime conn n = bench "SelectTime" $ nfIO $ do
+  vss <- quickQuery' conn "SELECT v5 FROM testSelect LIMIT ?" [SqlInt32 (fromIntegral n)]
+  commit conn
+  if all (\[v] -> (fromSql v :: LocalTime) == testTime) vss
+    then return ()
+    else error "benchSelectTime: Unexpected Time!"
 
 teardownSelect :: IConnection conn => conn -> IO ()
 teardownSelect conn = do
